e6bc790a6de0b3c9cecedfcfa7a779b414fe2af1
Nguyễn Văn Lượng <luonglpbl02@gmail.com>
2016-12-09 15:57:42 +0700

    Hoan thanh

---
+++ Android-Navigation-Drawer-master/app/src/main/java/youtube/demo/youtubedemo/SlideShow.java
@@ -0,0 +1,13 @@
+package youtube.demo.youtubedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlideShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_show);
+    }
+}
